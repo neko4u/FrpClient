9e08a138bda915226ac1ddd141a731454952ffc2
neko4u <345307827@qq.com>
2026-04-14 23:00:46 +0800

    处理双窗口问题

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+
 from ui.login_window import LoginWindow
+from ui.main_window import MainWindow
+from core.token_storage import TokenStorage
+from core.token_holder import TokenHolder
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    token = TokenStorage.load()
 
-    win = LoginWindow()
-    win.show()
+    if token:
+        TokenHolder.set_token(token, 0)
+        window = MainWindow()
+    else:
+        window = LoginWindow()
+
+    window.show()
 
     sys.exit(app.exec())
